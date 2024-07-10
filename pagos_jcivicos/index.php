7c49594526b6575b7b98dcbf0cb076c1b8efb73d
Pato Arteaga <lartz12.14@gmail.com>
2024-07-10 15:43:45 -0600

    10-07-2024_tarde
    Se creo la estructura de folders y el front controller

---
+++ pagos_jcivicos/index.php
@@ -0,0 +1,27 @@
+<?php
+  require_once ("./models/mdl.conexion.php");
+  //$con=new Conection();
+  //$con->conect();
+
+if(!isset($_GET['c'])){
+    //echo "No se asigno valor a la variable"."<br>";
+    require_once ("./controllers/inicio.controlador.php");
+    $controlador=new InicioControlador();
+    call_user_func(array($controlador,"Inicio"));
+
+  
+    }
+
+else{
+    echo"Valor de la variable controlador: ".$_GET['c'];
+
+    $controlador= $_GET['c'];
+    require_once("./controllers/$controlador.controlador.php");
+
+    $controlador = ucwords($controlador)."controlador";
+    $controlador = new $controlador;
+    
+    $accion = isset($_GET['a']) ? $_GET['c'] : "Inicio";
+    call_user_func(array($controlador,$accion));
+  }
+?>
