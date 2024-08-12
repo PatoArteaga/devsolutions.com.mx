0fbca2e6a032a185cb662771d16648dea7a20f86
Pato Arteaga <lartz12.14@gmail.com>
2024-08-11 23:41:12 -0600

    11-08-2024_Noche
    Se comenzó el proyecto de e-comerce agrodev.com

---
+++ agrodev/frontend/controllers/controller.plantilla.php
@@ -0,0 +1,8 @@
+<?php
+    class ControllerPlantilla{
+        public function Plantilla(){
+            include ("./views/plantilla.php");
+        }
+
+    }
+?>
--- agrodev/frontend/index.php
+++ agrodev/frontend/index.php
@@ -1,26 +1,5 @@
 <?php
-require_once "controllers/template.ctrl.php";
-$plantilla = new CrtPlantilla();
-$plantilla -> plantilla();
-    $plantilla -> suma(4,3);
+    require_once("controllers/controller.plantilla.php");
+    $plantilla=new ControllerPlantilla();
+    $plantilla->Plantilla();
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>hola: </div>  
-    <?php
-    
-
-    ?>
-    
-</body>
-</html>
-
-
