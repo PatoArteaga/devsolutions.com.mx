b090ae25e1375a1587ae0c2f90018bfe267d434f
Pato Arteaga <lartz12.14@gmail.com>
2025-09-01 22:56:35 -0600

    01_09_2025_Noche
    Se crearon las vistas de home, se modularizo el menu lateral (Side bar),   se creo el menu de "Nuevo registro" y "Consultar Registro"

---
+++ tramitesmejora/view/MainMenu/MainMenu.php
@@ -0,0 +1,26 @@
+<div class="content-side content-side-full">
+    <ul class="nav-main">
+        <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">Menu</span></li>
+        <li>
+            <a href="bd_dashboard.html"><i class="si si-puzzle"></i><span class="sidebar-mini-hide">Nuevo Registro</span></a>
+            <!-- <ul>
+                <li>
+                    <a href="be_pages_dashboard.html">Dashboard 1</a>
+                </li>
+                <li>
+                    <a href="be_pages_dashboard2.html">Dashboard 2</a>
+                </li>
+                <li>
+                    <a href="be_pages_dashboard3.html">Dashboard 3</a>
+                </li>
+                <li>
+                    <a href="be_pages_dashboard4.html">Dashboard 4</a>
+                </li>
+            </ul> -->
+        </li>
+        <li>
+            <a href="bd_dashboard.html"><i class="si si-compass"></i><span class="sidebar-mini-hide">Consultar Registro</span></a>
+        </li>
+        
+    </ul>
+</div>
